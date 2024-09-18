4b36c7a74af15f5d25f3306daff8e5691a3c69a9
Danilo Paulino da Silva <danilopaulinodasilva@gmail.com>
2024-09-18 07:08:37 -0300

    it works now??

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,13 @@
 import { defineConfig } from "vite";
-import { createHtmlPlugin } from "vite-plugin-html";
+import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [
-    createHtmlPlugin({
-      minify: true,
-      pages: [
-        {
-          entry: "src/main.js", // Arquivo de entrada principal
-          filename: "index.html", // Página principal
-        },
-        {
-          entry: "src/main.js", // Pode ser o mesmo arquivo de entrada
-          filename: "obrigado.html", // Arquivo de obrigado
-          template: "public/obrigado.html", // O template de origem
-        },
-      ],
-    }),
-  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        obrigado: resolve(__dirname, "obrigado.html"),
+      },
+    },
+  },
 });
